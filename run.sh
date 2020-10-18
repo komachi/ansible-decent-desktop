e0732d770ef2c99025f7439aa399378858b68052
Anton Nesterov <anton@nesterov.cc>
2020-10-18 15:16:41 +0000

    Configure audio Install gamemode-daemon Install lutris from repo, add configs Upgrade systemd and other from backports Upgrade dependent playbooks

--- run.sh
+++ run.sh
@@ -4,5 +4,10 @@ if [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-sudo apt install python3-jmespath python3-passlib python3-lxml python-libvirt ansible
+if [ $(lsb_release -sc) != "buster" ]; then
+   echo "This playbook was tested only on debian buster. Something can broke."
+fi
+
+sudo apt install -t buster-backports ansible
+sudo apt install python3-jmespath python3-passlib python3-lxml python-libvirt
 ansible-playbook --extra-vars username="$USER" --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 main.yml
